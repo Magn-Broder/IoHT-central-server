c995374fd80e1105601804a6e8fa3ec1f01873ba
Magn-Broder <89405705+Magn-Broder@users.noreply.github.com>
2023-12-20 16:43:32 +0100

    Update log_bpm.py

--- log_bpm.py
+++ log_bpm.py
@@ -5,13 +5,12 @@ import ssl
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    client.subscribe("BPM")  # Replace 'your_topic' with the desired MQTT topic
+    client.subscribe("BPM")
 
 def on_message(client, userdata, msg):
     received_data = msg.payload.decode("utf-8").split(',')
     received_bpm, received_bpm_hash = received_data[0], received_data[1]
 
-    # TODO: Create a hash value of received_bpm using SHA256
     calculated_hash = hashlib.sha256(str(received_bpm).encode()).hexdigest()
 
     if received_bpm_hash == calculated_hash:
@@ -20,28 +19,22 @@ def on_message(client, userdata, msg):
     else:
         print("Hash mismatch. Data not logged.")
 
-# Replace the following placeholders with your actual MQTT broker details
-broker_address = "192.168.1.5"  # Example: "mqtt.example.com"
+broker_address = "192.168.1.5"
 port = 8883
 username = "esp_client"
 password = "mqttclient123"
-ca_file = "ca.crt"  # Replace with the actual path to your CA certificate file
+ca_file = "ca.crt"
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Set the username and password for authentication
 client.username_pw_set(username, password)
 
-# Set up SSL/TLS context
 ssl_context = ssl.create_default_context(cafile=ca_file)
 
-# Set TLS context for the client (disable certificate verification)
 client.tls_set(ca_certs=ca_file, cert_reqs=ssl.CERT_NONE)
 
-# Connect to the broker
 client.connect(broker_address, port, 60)
 
-# Start the MQTT client loop (this function runs in the background)
 client.loop_forever()
